aa376d00b45c6865bd76781365031b2e6797efb2
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-08-27 14:02:01 +1000

    loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,14 @@
 
 	const isOpenedFromTelegram = Boolean(window.Telegram.WebView.initParams.tgWebAppThemeParams)
 	let userDataLoaded = false
-	
+
 	fetchData('getMe').then((data) => {
 		userData.set(data)
 		userDataLoaded = true
 	})
 
 	fetchData('updateProfilePhoto')
-	
+
 	let theme = window.Telegram.WebApp.colorScheme
 	$: {
 		document.body.classList.toggle('dark', theme === 'dark')
@@ -31,8 +31,27 @@
 {#if isOpenedFromTelegram}
 	{#if !userDataLoaded}
 		<div class="loader" out:fade>
-			<svg width="96" height="119" viewBox="0 0 64 79" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M64 49.9692C64 61.7846 59.0769 78.7692 32 78.7692C4.92308 78.7692 0 61.7846 0 49.9692C0 39.3846 5.88719 36.4308 6.15386 26.8308C6.30423 21.4175 4.92309 19.6923 4.92309 18.7077C4.92309 17.7231 5.4154 17.2308 6.40001 17.2308C10.5846 17.2308 16.7385 23.6308 18.7077 26.8308C28.5538 14.5231 27.0769 4.92308 27.0769 1.47692C27.0769 0.984615 27.0769 0 28.0615 0C33.9692 0 48.4923 16.9846 50.7077 31.2615C53.9077 29.2923 56.1231 24.6154 57.6 24.6154C60.1938 24.6154 64 38.1538 64 49.9692Z"/>
+			<svg
+				width="95.25mm"
+				height="47.625mm"
+				viewBox="0 0 95.25 47.625"
+				version="1.1"
+				id="svg186"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:svg="http://www.w3.org/2000/svg"
+			>
+				<defs id="defs183">
+					<rect x="-238.49207" y="56.118542" width="367.09445" height="123.88146" id="rect352" />
+				</defs>
+				<g id="layer1" transform="translate(6.1183991,-142.9996)">
+					<text
+						xml:space="preserve"
+						transform="matrix(0.68816381,0,0,0.68816381,79.439094,121.62535)"
+						id="text350"
+						style="font-weight:bold;font-size:16px;line-height:1.25;font-family:Agenda;-inkscape-font-specification:'Agenda Bold';text-align:center;text-decoration-color:#000000;white-space:pre;shape-inside:url(#rect352);display:inline;fill:#1a1a1a;stroke-width:3.80578;stroke-linecap:round;stroke-linejoin:round;stop-color:#000000"
+						><tspan x="-107.09732" y="71.294781" id="tspan522">Чистая Лига</tspan></text
+					>
+				</g>
 			</svg>
 		</div>
 	{/if}
