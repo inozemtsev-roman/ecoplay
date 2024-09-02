d99174448d733f245ec0ceaff8aa220fb91a364c
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-09-02 15:46:52 +1000

    tag

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -3,7 +3,7 @@
 
 	$: {
 		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-EQ9VX3XVED', {
+			gtag('config', 'G-NCVKVBX7X5', {
 				page_title: document.title,
 				page_path: $page.url.pathname
 			})
@@ -12,7 +12,7 @@
 </script>
 
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ9VX3XVED"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-NCVKVBX7X5"></script>
 	<script>
 		window.dataLayer = window.dataLayer || []
 		function gtag() {
@@ -20,6 +20,6 @@
 		}
 		gtag('js', new Date())
 
-		gtag('config', 'G-EQ9VX3XVED')
+		gtag('config', 'G-NCVKVBX7X5')
 	</script>
 </svelte:head>
