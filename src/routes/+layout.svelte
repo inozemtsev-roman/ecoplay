2143420c32b915122d8ef1da92dede544eac375b
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-09-05 09:25:36 +1000

    bc gr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -653,7 +653,7 @@
 		top: 0;
 		width: 100%;
 		height: var(--tg-viewport-height);
-		background: url('/recycling.jpg');
+		background: #434343;
 		display: flex;
 		align-items: center;
 		justify-content: center;
